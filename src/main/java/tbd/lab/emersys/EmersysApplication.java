405e2f031d94cdf49c801dd7ccdb7d0ca39f0000
Charlie <carlos.henriquez.r@usach.cl>
2020-05-16 19:42:06 -0400

    Real initial commit

---
+++ src/main/java/tbd/lab/emersys/EmersysApplication.java
@@ -0,0 +1,13 @@
+package tbd.lab.emersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmersysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmersysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tbd/lab/emersys/EmersysApplicationTests.java
@@ -0,0 +1,13 @@
+package tbd.lab.emersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmersysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
