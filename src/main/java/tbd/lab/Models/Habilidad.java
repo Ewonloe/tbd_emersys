ed595bf1d432f37b495c805d75d9291fe0b683a6
Dany-Ocampo <obsessionxrobert@live.cl>
2020-05-27 01:28:02 -0400

    Update Habilidad.java
    Arreglé los errores que tiraba Habilidad (Modelo).

--- src/main/java/tbd/lab/Models/Habilidad.java
+++ src/main/java/tbd/lab/Models/Habilidad.java
@@ -1,19 +1,11 @@
 package tbd.lab.Models;
 
-import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.sql.Date;
 
-@Entity
-@Data
 public class Habilidad {
 
-	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
+    private BigDecimal id;
     private String descripcion;
 
 }
