8907e0917b60bb2d48ee1e6966089dd52c353f04
Dany-Ocampo <obsessionxrobert@live.cl>
2020-06-03 18:59:08 -0400

    Create HabilidadService.java

---
+++ src/main/java/tbd/lab/services/HabilidadService.java
@@ -0,0 +1,12 @@
+package tbd.lab.services;
+
+import org.springframework.web.bind.annotation.*;
+import tbd.lab.Models.Voluntario;
+import tbd.lab.Repositories.VoluntarioRepository;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class HabilidadService {
+}
