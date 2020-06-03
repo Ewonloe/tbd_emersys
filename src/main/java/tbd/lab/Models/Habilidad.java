6434b5adec462f74616393ae93a04c45aa4a6e9b
Dany-Ocampo <obsessionxrobert@live.cl>
2020-06-03 18:32:09 -0400

    Update Habilidad.java
    Modelo Habilidad

--- src/main/java/tbd/lab/Models/Habilidad.java
+++ src/main/java/tbd/lab/Models/Habilidad.java
@@ -8,4 +8,19 @@ public class Habilidad {
     private BigDecimal id;
     private String descripcion;
 
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
