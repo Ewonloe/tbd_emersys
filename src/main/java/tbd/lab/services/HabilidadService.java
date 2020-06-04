3d70434d9749b389f63dc9eada30705a8004b5f8
Dany-Ocampo <obsessionxrobert@live.cl>
2020-06-03 21:58:10 -0400

    Update HabilidadService.java

--- src/main/java/tbd/lab/services/HabilidadService.java
+++ src/main/java/tbd/lab/services/HabilidadService.java
@@ -1,12 +1,16 @@
 package tbd.lab.services;
 
 import org.springframework.web.bind.annotation.*;
-import tbd.lab.Models.Voluntario;
-import tbd.lab.Repositories.VoluntarioRepository;
+import tbd.lab.Models.Habilidad;
+import tbd.lab.Repositories.HabilidadRepository;
 
 import java.util.List;
 
 @CrossOrigin
 @RestController
 public class HabilidadService {
+    private final HabilidadRepository habiRepo {
+
+
+    }
 }
